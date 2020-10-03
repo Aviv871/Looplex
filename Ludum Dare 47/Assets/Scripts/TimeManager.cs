ea1a86380ae5d880f09d9490cbe8c0425afb5503
Aviv <21aviv12@gmail.com>
2020-10-03 03:29:18 +0300

    Time Manager code

---
+++ Ludum Dare 47/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public static TimeManager timeManagerInstance;
+
+    // The action to be executated at the event, takes the delta since the wanted event time to now
+    public delegate void TimeEventFunc(float timeDelta);
+    private Dictionary<float, List<TimeEventFunc>> timeEvents = new Dictionary<float, List<TimeEventFunc>>();
+
+    private void Awake()
+    {
+        if (timeManagerInstance != null) Debug.LogError("More than 1 timeManagerInstance in scene! Use only one");
+        timeManagerInstance = this;
+        timeEvents.Clear();
+    }
+
+    public void RegisterTimeEvent(float time, TimeEventFunc action)
+    {
+        if (timeEvents.ContainsKey(time))
+        {
+            timeEvents[time].Add(action);
+        }
+        else
+        {
+            List<TimeEventFunc> newList = new List<TimeEventFunc> { action };
+            timeEvents.Add(time, newList);
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        handleTimeEvents();
+    }
+
+    private void handleTimeEvents()
+    {
+        List<float> keysToRemove = new List<float>();
+
+        foreach (var item in timeEvents)
+        {
+            if (item.Key < Time.timeSinceLevelLoad)
+            {
+                keysToRemove.Add(item.Key);
+                foreach (var action in item.Value)
+                {
+                    action(Time.timeSinceLevelLoad - item.Key);
+                }
+            }
+        }
+
+        foreach (var keyToRemove in keysToRemove)
+        {
+            timeEvents.Remove(keyToRemove);
+        }
+    }
+}
