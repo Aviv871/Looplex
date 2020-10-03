4c31c0d4d9532ecddd70e6cdc9af8ba437806df3
Aviv <21aviv12@gmail.com>
2020-10-03 04:15:42 +0300

    Pixel prefect setting + animations left and right

---
+++ Ludum Dare 47/Assets/Scripts/SnapToGrid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToGrid : MonoBehaviour
+{
+    public float PPU = 32; // pixels per unit (your tile size)
+
+    private void LateUpdate()
+    {
+        Vector3 position = transform.localPosition;
+
+        position.x = (Mathf.Round(transform.parent.position.x * PPU) / PPU) - transform.parent.position.x;
+        position.y = (Mathf.Round(transform.parent.position.y * PPU) / PPU) - transform.parent.position.y;
+
+        transform.localPosition = position;
+    }
+}
