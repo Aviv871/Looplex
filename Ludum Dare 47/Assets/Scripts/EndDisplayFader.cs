4c478286bbf978f7107601a43a2bcf6dcac56504
Aviv <21aviv12@gmail.com>
2020-10-05 22:54:12 +0300

    Fading end display

---
+++ Ludum Dare 47/Assets/Scripts/EndDisplayFader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndDisplayFader : MonoBehaviour
+{
+    [SerializeField] private Image[] images = null;
+    [SerializeField] private Text[] texts = null;
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float a = 0f;
+        while (a < 1f)
+        {
+            foreach (Image image in images)
+            {
+                image.color = new Color(image.color.r, image.color.g, image.color.b, a);
+            }
+            foreach (Text text in texts)
+            {
+                text.color = new Color(text.color.r, text.color.g, text.color.b, a);
+            }
+
+            a += Time.deltaTime;
+            yield return 0;
+        }
+    }
+}
